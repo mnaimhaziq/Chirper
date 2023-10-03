d5e5c46dcc403eff9ba316602935b15531b7c7b4
mnaimhaziq <101214356+mnaimhaziq@users.noreply.github.com>
2023-10-04 07:26:45 +0800

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <section>
+        <h1 className='head-text'>
+            communities
+        </h1>
+    </section>
+  )
+}
+
+export default Page
