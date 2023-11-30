06b338eaba2cc322208bc03034a30babf1f3a0d1
meku <71602023+mekukun@users.noreply.github.com>
2023-11-30 22:55:20 +0800

    Adding dependencies on MUI Icon
    - Using icons and list components from `MUI`.
    - Adjusting spacing between `ListItemIcon` and `ListItemText`

--- components/ui/ShareButton.tsx
+++ components/ui/ShareButton.tsx
@@ -4,9 +4,32 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Popper from "@mui/material/Popper";
 import Image from "next/image";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 import { ClickAwayListener } from "@mui/base/ClickAwayListener";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function ShareButton() {
+  const theme = createTheme({
+    components: {
+      // Name of the component
+      MuiListItemIcon: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            minWidth: 40,
+          },
+        },
+      },
+    },
+  });
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -32,9 +55,28 @@ export default function ShareButton() {
           onClick={handleClick}
         />
 
-        <Popper id={id} open={open} anchorEl={anchorEl}>
-          <Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>
-            Sharing Tiles
+        <Popper id={id} open={open} anchorEl={anchorEl} sx={{ width: 250 }}>
+          <Box sx={{ bgcolor: "background.paper" }}>
+            <ThemeProvider theme={theme}>
+              <List>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <InboxIcon sx={{ fontSize: 20 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Copy link" />
+                  </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <DraftsIcon sx={{ fontSize: 20 }} />
+                    </ListItemIcon>
+                    <ListItemText primary="Share post via..." />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </ThemeProvider>
           </Box>
         </Popper>
       </div>
