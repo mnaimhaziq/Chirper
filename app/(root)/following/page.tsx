fd8dd508050c50162394fdbf0f69c3b71610b2d5
mnaimhaziq <101214356+mnaimhaziq@users.noreply.github.com>
2024-01-12 17:16:52 +0800

    Followiing and For you Page

--- app/(root)/following/page.tsx
+++ app/(root)/following/page.tsx
@@ -1,33 +1,29 @@
-
-
 import ThreadCard from "@/components/cards/ThreadCard";
-import { fetchPosts } from "@/lib/actions/thread.action";
+import { fetchPosts, fetchPostsForFollowing } from "@/lib/actions/thread.action";
 import { UserButton, currentUser } from "@clerk/nextjs";
 import ToggleButtons from "@/components/ui/ToggleButtons";
+
 export default async function Home() {
   const user = await currentUser();
   if (!user) return null;
 
-  const result = await fetchPosts(1, 30);
+  const result = await fetchPostsForFollowing(user.id,1, 30);
 
   return (
     <>
-       <ToggleButtons />
-      <h1 className="head-text text-left dark:text-light-1 text-dark-1">Home</h1>
+      <ToggleButtons />
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No Threads Found</p>
         ) : (
           <>
-            {result.posts.map((post : any) => (
+            {result.posts.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
                 currentUserId={user.id}
                 parentId={post.parentId}
-                mediaLink={post.mediaLink}
-                quoteId={post.quoteId}
                 content={post.text}
                 author={post.author}
                 community={post.community}
